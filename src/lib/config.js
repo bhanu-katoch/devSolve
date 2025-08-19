2f7c84ef8a8d7e7487247a5a6917f8a9500fbc54
Bhanu <bhanukatoch445@gmail.com>
2025-08-19 11:17:48 +0530

    otp sent and recieve successfully for junior registration

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,4 @@
 const conf = {
     appwrite_url : import.meta.env.VITE_APPWRITE_ENDPOINT,
-    database_id : import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    project_id : import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    collection_projectLeadRegister : import.meta.env.VITE_APPWRITE_PROJECTLEAD_COLLECTION_ID,
-    collection_juniorRegister : import.meta.env.VITE_APPWRITE_JUNIOR_COLLECTION_ID,
 }
 export default conf
